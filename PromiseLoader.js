e9156b640fd9f15d72dda047683dab384d68ab49
Lindsey Linck <lindsey@conceptone.com.br>
2023-05-03 21:32:43 -0300

    Fix: Renaming the script Feat: Allowing costum attributes

---
+++ PromiseLoader.js
@@ -0,0 +1,37 @@
+/**
+ * Loads an script into the document using a promise atop of onLoad/onError
+ * events, allowing all promise operations over dynamic loading.
+ * 
+ * @param {String} Src Url of the script
+ * @param {Boolean} Head If the script must be appended to document head (default: true)
+ * @param {Object<String, String>} Attributes Extra attributes for the script tag
+ * @returns {Promise<Event>}
+ */
+function PromiseLoad (Src, Head, Attributes) {
+    return new Promise((onResolve, onReject) => {
+        /**
+         * @type {HTMLElement}
+         */
+        const Container = (Head || true)
+            ? document.head
+            : document.body
+        ;
+        
+        const Script = document.createElement('script');
+        if (typeof Attributes === "object" && Attributes !== null) {
+            for (const Name in Attributes) {
+                Script.setAttribute(Name, Attributes[Name]);
+            }
+        }
+
+        Script.addEventListener("error", (Event) => {
+            console.error(Event);
+
+            onReject(Event);
+        });
+        Script.addEventListener("load", onResolve);
+        Script.src = Src;
+
+        Container.appendChild(Script);
+    });
+};
